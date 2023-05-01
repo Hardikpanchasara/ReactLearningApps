f71bc6ef95f3d23754e809aa4b48696d1e505dff
Hardikpanchasara <hardikpatel97253@gmail.com>
2023-05-01 13:30:14 +0530

    update tops app

--- Tops-react-app/src/component/FunctionComponent/FunctionComponentMenu.jsx
+++ Tops-react-app/src/component/FunctionComponent/FunctionComponentMenu.jsx
@@ -5,35 +5,39 @@ import { MDBBtn } from 'mdb-react-ui-kit'
 const FunctionComponentMenu = () => {
   return (
     <>
-      <ul>
-        <li className="d-block" >
-          <Link to="Functionintro"><MDBBtn className='my-2' color="secondary" > Function component Intro </MDBBtn></Link>
-        </li>
-        <li className="d-block" >
-          <Link to="usestate"><MDBBtn className='my-2' color="secondary" > UseState </MDBBtn></Link>
-        </li>
-        <li className="d-block" >
-          <Link to="useeffect"><MDBBtn className='my-2' color="secondary" > UseEffect </MDBBtn></Link>
-        </li>
-        <li className="d-block" >
-          <Link to="usecontext"><MDBBtn className='my-2' color="secondary" > UseContext </MDBBtn></Link>
-        </li>
-        <li className="d-block" >
-          <Link to="usereducer"><MDBBtn className='my-2' color="secondary" > UseReducer </MDBBtn></Link>
-        </li>
-        <li className="d-block" >
-          <Link to="useref"><MDBBtn className='my-2' color="secondary" > UseRef </MDBBtn></Link>
-        </li>
-        <li className="d-block" >
-          <Link to="uselayouteffect"><MDBBtn className='my-2' color="secondary" > UseLayoutEffect </MDBBtn></Link>
-        </li>
-        <li className="d-block" >
-          <Link to="usememo"><MDBBtn className='my-2' color="secondary" > UseMemo </MDBBtn></Link>
-        </li>
-        <li className="d-block" >
-          <Link to="usecallback"><MDBBtn className='my-2' color="secondary" > UseCallBack </MDBBtn></Link>
-        </li>
-      </ul>
+      <div className="row">
+        <div className="col-6 offset-6">
+          <ul>
+            <li className="d-block" >
+              <Link to="Functionintro"><MDBBtn className='my-2' color="secondary" > Function component Intro </MDBBtn></Link>
+            </li>
+            <li className="d-block" >
+              <Link to="usestate"><MDBBtn className='my-2' color="secondary" > UseState </MDBBtn></Link>
+            </li>
+            <li className="d-block" >
+              <Link to="useeffect"><MDBBtn className='my-2' color="secondary" > UseEffect </MDBBtn></Link>
+            </li>
+            <li className="d-block" >
+              <Link to="usecontext"><MDBBtn className='my-2' color="secondary" > UseContext </MDBBtn></Link>
+            </li>
+            <li className="d-block" >
+              <Link to="usereducer"><MDBBtn className='my-2' color="secondary" > UseReducer </MDBBtn></Link>
+            </li>
+            <li className="d-block" >
+              <Link to="useref"><MDBBtn className='my-2' color="secondary" > UseRef </MDBBtn></Link>
+            </li>
+            <li className="d-block" >
+              <Link to="uselayouteffect"><MDBBtn className='my-2' color="secondary" > UseLayoutEffect </MDBBtn></Link>
+            </li>
+            <li className="d-block" >
+              <Link to="usememo"><MDBBtn className='my-2' color="secondary" > UseMemo </MDBBtn></Link>
+            </li>
+            <li className="d-block" >
+              <Link to="usecallback"><MDBBtn className='my-2' color="secondary" > UseCallBack </MDBBtn></Link>
+            </li>
+          </ul>
+        </div>
+      </div>
       <Outlet />
     </>
   )
