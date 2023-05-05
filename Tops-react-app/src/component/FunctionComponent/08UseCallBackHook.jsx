85213a99a0cebc15db3773997923c5ac640baeaa
Hardikpanchasara <hardikpatel97253@gmail.com>
2023-05-05 10:47:49 +0530

    usecallbackhook add

--- Tops-react-app/src/component/FunctionComponent/08UseCallBackHook.jsx
+++ Tops-react-app/src/component/FunctionComponent/08UseCallBackHook.jsx
@@ -1,33 +1,53 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 
+
+const funccount = new Set()
 const UseCallBackHook = () => {
 
-  // const clickHandler = () => {
-  //   console.log("called without hook")
-  //   fetch('https://jsonplaceholder.typicode.com/users/1')
-  //     .then(response => response.json())
-  //     .then(json => console.log(json))
-  // }
+  const [count, setCount] = useState(0)
+  const [number, setNumber] = useState(0)
 
-  // const cachedFn = useCallback(fn, dependencies)  syntaxt
-  const clickHandler = useCallback(() => {
-      console.log("called with callback hook")
-      fetch('https://jsonplaceholder.typicode.com/users/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
-    } , []) 
-  const ButtonWrapper = (({CallbackfnHanlder}) => {
-    console.log("clicked")
-    return <button onClick={CallbackfnHanlder}>click</button>
-    
-  })
+  //  const incrementcount = () => {
+  //   setCount(count + 1)
+  //  }
+  //  const decrementcount = () => {
+  //   setCount(count + 1)
+  //  }
+  //  const incrementnumber = () => {
+  //   setNumber(number + 1)
+  //  }
+
+
+  const incrementcount = useCallback(() => {
+    setCount(count + 1)
+  }, [count])
+  const decrementcount = useCallback(() => {
+    setCount(count - 1)
+  }, [count])
+  const incrementnumber = useCallback(() => {
+    setNumber(number + 1)
+  }, [number])
+
+  funccount.add(incrementcount)
+  funccount.add(decrementcount)
+  funccount.add(incrementnumber)
+  console.log(funccount.size)
+  console.log(funccount)
+
   return (
     <div className='container'>
       <h3>useCallback is a React Hook that lets you cache a function definition between re-renders.</h3>
       <h3>You should only rely on useCallback as a performance optimization.</h3>
       <p> The array of dependencies is not passed as arguments to the callback. Conceptually, though, that's what they represent: every value referenced inside the callback should also appear in the dependencies array. In the future, a sufficiently advanced compiler could create this array automatically.</p>
 
-      <ButtonWrapper CallbackfnHanlder={clickHandler} />
+      <div className="my-5">
+        count : {count}
+        number : {number}
+        <button onClick={incrementcount}>incrementcount</button>
+        <button onClick={decrementcount}>decrementcount</button>
+        <button onClick={incrementnumber}>incrementnumber</button>
+      </div>
+
       <br /><br /><br />
     </div>
   )
